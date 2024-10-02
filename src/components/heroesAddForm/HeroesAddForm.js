c66dcd604f55288393f5ee521383c88d103aa1aa
Andrii <andriy2003po@gmail.com>
2024-10-03 00:06:40 +0200

    prettiered

--- src/components/heroesAddForm/HeroesAddForm.js
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -1,79 +1,96 @@
-import { v4 as uuidv4 } from 'uuid'
-import { Formik, Field, Form, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
-import { useHttp } from '../../hooks/http.hook';
-import { fetchHeroes } from '../heroesList/heroesSlice';
-import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from "uuid";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { useHttp } from "../../hooks/http.hook";
+import { fetchHeroes } from "../heroesList/heroesSlice";
+import { useDispatch } from "react-redux";
 
 const HeroesAddForm = () => {
-    const {request} = useHttp()
-    const dispatch = useDispatch()
-    return (
-        <Formik
-            initialValues={{
-                name: '',
-                text: '',
-                element: ''
-            }}
-            validationSchema={Yup.object({
-                name: Yup.string().min(2, 'Must be 2 characters or more').required('Required'),
-                text: Yup.string().min(10, 'Must be 10 characters or more').required('Required'),
-                element: Yup.string().required('Required')
-            })}
-            onSubmit={values => {
-                const body = {
-                    id: uuidv4(),
-                    ...values
-                }
-                request(`http://localhost:3001/heroes`, 'POST', JSON.stringify(body))
-                    .then(() => dispatch(fetchHeroes(request)))
-            }}
-        >
-            <Form className="border p-4 shadow-lg rounded">
-                <div className="mb-3">
-                    <label htmlFor="name" className="form-label fs-4">Имя нового героя</label>
-                    <Field
-                        type="text" 
-                        name="name" 
-                        className="form-control" 
-                        id="name" 
-                        placeholder="Как меня зовут?"/>
-                    <ErrorMessage name="name" className='error' component={'div'} />
+  const { request } = useHttp();
+  const dispatch = useDispatch();
+  return (
+    <Formik
+      initialValues={{
+        name: "",
+        text: "",
+        element: "",
+      }}
+      validationSchema={Yup.object({
+        name: Yup.string()
+          .min(2, "Must be 2 characters or more")
+          .required("Required"),
+        text: Yup.string()
+          .min(10, "Must be 10 characters or more")
+          .required("Required"),
+        element: Yup.string().required("Required"),
+      })}
+      onSubmit={(values) => {
+        const body = {
+          id: uuidv4(),
+          ...values,
+        };
+        request(
+          `http://localhost:3001/heroes`,
+          "POST",
+          JSON.stringify(body)
+        ).then(() => dispatch(fetchHeroes(request)));
+      }}
+    >
+      <Form className="border p-4 shadow-lg rounded">
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label fs-4">
+            Имя нового героя
+          </label>
+          <Field
+            type="text"
+            name="name"
+            className="form-control"
+            id="name"
+            placeholder="Как меня зовут?"
+          />
+          <ErrorMessage name="name" className="error" component={"div"} />
+        </div>
 
-                </div>
+        <div className="mb-3">
+          <label htmlFor="text" className="form-label fs-4">
+            Описание
+          </label>
+          <Field
+            as="textarea"
+            name="text"
+            className="form-control"
+            id="text"
+            placeholder="Что я умею?"
+            style={{ height: "130px" }}
+          />
+          <ErrorMessage name="text" className="error" component={"div"} />
+        </div>
 
-                <div className="mb-3">
-                    <label htmlFor="text" className="form-label fs-4">Описание</label>
-                    <Field
-                        as="textarea"
-                        name="text" 
-                        className="form-control" 
-                        id="text" 
-                        placeholder="Что я умею?"
-                        style={{"height": '130px'}}/>
-                    <ErrorMessage name="text" className='error' component={'div'} />
-                </div>
+        <div className="mb-3">
+          <label htmlFor="element" className="form-label">
+            Выбрать элемент героя
+          </label>
+          <Field
+            as="select"
+            className="form-select"
+            id="element"
+            name="element"
+          >
+            <option>Я владею элементом...</option>
+            <option value="fire">Огонь</option>
+            <option value="water">Вода</option>
+            <option value="wind">Ветер</option>
+            <option value="earth">Земля</option>
+          </Field>
+          <ErrorMessage name="element" className="error" component={"div"} />
+        </div>
 
-                <div className="mb-3">
-                    <label htmlFor="element" className="form-label">Выбрать элемент героя</label>
-                    <Field 
-                        as='select'
-                        className="form-select" 
-                        id="element"  
-                        name="element">
-                        <option >Я владею элементом...</option>
-                        <option value="fire">Огонь</option>
-                        <option value="water">Вода</option>
-                        <option value="wind">Ветер</option>
-                        <option value="earth">Земля</option>
-                    </Field>
-                    <ErrorMessage name="element" className='error' component={'div'} />
-                </div>
-
-                <button type="submit" className="btn btn-primary">Создать</button>
-            </Form>
-        </Formik>
-    )
-}
+        <button type="submit" className="btn btn-primary">
+          Создать
+        </button>
+      </Form>
+    </Formik>
+  );
+};
 
 export default HeroesAddForm;
--- src/components/heroesListItem/HeroesListItem.js
+++ src/components/heroesListItem/HeroesListItem.js
@@ -1,61 +1,64 @@
 import { useDispatch } from "react-redux";
-import { fetchHeroes } from '../heroesList/heroesSlice';
-import {useHttp} from '../../hooks/http.hook';
-
+import { fetchHeroes } from "../heroesList/heroesSlice";
+import { useHttp } from "../../hooks/http.hook";
 
 const HeroesListItem = ({ name, text, element, id }) => {
-	let elementClassName;
+  let elementClassName;
 
-	switch (element) {
-		case "fire":
-			elementClassName = "bg-danger bg-gradient";
-			break;
-		case "water":
-			elementClassName = "bg-primary bg-gradient";
-			break;
-		case "wind":
-			elementClassName = "bg-success bg-gradient";
-			break;
-		case "earth":
-			elementClassName = "bg-secondary bg-gradient";
-			break;
-		default:
-			elementClassName = "bg-warning bg-gradient";
-	}
-	
-	const { request } = useHttp()
+  switch (element) {
+    case "fire":
+      elementClassName = "bg-danger bg-gradient";
+      break;
+    case "water":
+      elementClassName = "bg-primary bg-gradient";
+      break;
+    case "wind":
+      elementClassName = "bg-success bg-gradient";
+      break;
+    case "earth":
+      elementClassName = "bg-secondary bg-gradient";
+      break;
+    default:
+      elementClassName = "bg-warning bg-gradient";
+  }
 
-	const dispatch = useDispatch()
-	const onDelete = () => {
-		console.log('delete', id)
-		request(`http://localhost:3001/heroes/${id}`, 'DELETE')
-		.then(() => dispatch(fetchHeroes()))
-	}
+  const { request } = useHttp();
 
-	return (
-		<li
-			className={`card flex-row mb-4 shadow-lg text-white ${elementClassName}`}
-			key={id}
-		>
-			<img
-				src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Unknown_person.jpg/1200px-Unknown_person.jpg"
-				className="img-fluid w-25 d-inline"
-				alt="unknown hero"
-				style={{ objectFit: "cover" }}
-			/>
-			<div className="card-body">
-				<h3 className="card-title">{name}</h3>
-				<p className="card-text">{text}</p>
-			</div>
-			<span className="position-absolute top-0 start-100 translate-middle badge border rounded-pill bg-light" onClick={(e) => onDelete(e)}>
-				<button
-					type="button"
-					className="btn-close btn-close"
-					aria-label="Close"
-				></button>
-			</span>
-		</li>
-	);
+  const dispatch = useDispatch();
+  const onDelete = () => {
+    console.log("delete", id);
+    request(`http://localhost:3001/heroes/${id}`, "DELETE").then(() =>
+      dispatch(fetchHeroes())
+    );
+  };
+
+  return (
+    <li
+      className={`card flex-row mb-4 shadow-lg text-white ${elementClassName}`}
+      key={id}
+    >
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Unknown_person.jpg/1200px-Unknown_person.jpg"
+        className="img-fluid w-25 d-inline"
+        alt="unknown hero"
+        style={{ objectFit: "cover" }}
+      />
+      <div className="card-body">
+        <h3 className="card-title">{name}</h3>
+        <p className="card-text">{text}</p>
+      </div>
+      <span
+        className="position-absolute top-0 start-100 translate-middle badge border rounded-pill bg-light"
+        onClick={(e) => onDelete(e)}
+      >
+        <button
+          type="button"
+          className="btn-close btn-close"
+          aria-label="Close"
+        ></button>
+      </span>
+    </li>
+  );
 };
 
 export default HeroesListItem;
